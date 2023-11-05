78a25343eeb4db895a4bb33520b4b0b810122d85
simonfoy <simonfoy1@proton.me>
2023-11-05 17:52:18 -0500

    Initial commit

---
+++ src/main/java/me/simonfoy/craftroyale/CraftRoyale.java
@@ -0,0 +1,17 @@
+package me.simonfoy.craftroyale;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftRoyale extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
